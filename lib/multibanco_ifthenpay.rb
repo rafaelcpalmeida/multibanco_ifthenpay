ffe97e4e9ce6853fd987a77c4f1f35ca427da997
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-04-09 15:31:24 +0100

    Simplify Multibanco class constructor

--- lib/multibanco_ifthenpay.rb
+++ lib/multibanco_ifthenpay.rb
@@ -11,15 +11,19 @@ module MultibancoIfthenpay
     # @param [String] order_id
     # @param [Float] order_value
     def initialize(entity, sub_entity, order_id, order_value)
-      raise ArgumentError, 'Not a valid entity' unless entity.length == 5
-      raise ArgumentError, 'Not a valid sub-entity' unless
-          !sub_entity.empty? && sub_entity.length < 4
-      raise ArgumentError, 'Not a valid order value' unless order_value >= 1
+      validate!(entity, sub_entity, order_value)
 
       @entity = entity
       @sub_entity = sub_entity
       @order_id = order_id
       @order_value = order_value
+    end
+
+    def validate!(entity, sub_entity, order_value)
+      raise ArgumentError, 'Not a valid entity' unless entity.length == 5
+      raise ArgumentError, 'Not a valid sub-entity' unless
+          !sub_entity.empty? && sub_entity.length < 4
+      raise ArgumentError, 'Not a valid order value' unless order_value >= 1
     end
 
     # @param [BooleanMatcher] prettify
